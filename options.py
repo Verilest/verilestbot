85c1a05c2465a1fec8e04baadb9baf45654ae73a
Rostislav <105703069+Verilest@users.noreply.github.com>
2023-08-19 12:40:12 +0300

    options.py
    options file

---
+++ options.py
@@ -0,0 +1,10 @@
+urlcat = 'https://aws.random.cat/meow'
+urlcat2 = 'https://cataas.com/cat'
+urldog = 'https://random.dog/woof.json'
+yesno = 'https://yesno.wtf/api'
+urlquestion = 'http://jservice.io/api/random?count=1'
+urlbored = 'http://www.boredapi.com/api/activity/'
+
+city_list = ('New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Moscow', 'Bryansk', 'Reutov', 'Balashiha', 'Sevastopol', 'Anapa')
+
+TOKEN = "MTA1ODA5Njg0MzA1OTM2Mzk2MQ.GhYr2j.-XbBpL7snCPhq-4GqvJsYo4_fESB9heCA93n_o"
